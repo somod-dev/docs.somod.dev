311792067b5ab3ad2483a583604c14d5b2c4dbf5
lokesh <loki.cse@gmail.com>
2023-03-14 17:28:09 +0530

    reading filepaths from _file

--- pages/[[...slug]].tsx
+++ pages/[[...slug]].tsx
@@ -1,7 +1,29 @@
 /* eslint-disable no-console */
+import { createReadStream } from "node:fs";
+import { createInterface } from "node:readline";
+
+async function processLineByLine() {
+  const fileStream = createReadStream("./node_modules/somod-docs/src/_files");
+  const paths = [{ params: { slug: ["1", "2"] } }];
+
+  const rl = createInterface({
+    input: fileStream
+  });
+  for await (const line of rl) {
+    console.log(`Line from file: ${line}`);
+    paths.push({ params: { slug: line.split("/") } });
+  }
+
+  return paths;
+}
+
 export async function getStaticPaths() {
+  const paths = await processLineByLine();
+
+  console.log("after line - " + paths);
+
   return {
-    paths: [{ params: { slug: ["1", "2"] } }, { params: { slug: ["3", "4"] } }],
+    paths: paths,
     fallback: false
   };
 }
@@ -13,8 +35,12 @@ export async function getStaticProps(context) {
   return { props: params };
 }
 
-export default function Page(props) {
-  console.log("props is - ");
-  console.log(props);
-  return <div>dfdfs</div>;
-}
+// export default function Page(props) {
+//   console.log("props is - ");
+//   console.log(props);
+//   return <div>dfdfs</div>;
+// }
+
+export { default } from "../demoUtils/emptyDemoComponent";
+
+// export const getStaticProps = getStaticPropsFactory("google-analytics");
